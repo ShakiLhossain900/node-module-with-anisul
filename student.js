1c69b908ced3a2a5c042f587baa9d0ddfc18109e
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-08-25 15:08:34 +0600

    Add files via upload

---
+++ student.js
@@ -0,0 +1,22 @@
+ exports.getName = () => {
+  return "Gazi Md shakil hossain" ;
+};
+const getAge = () => {
+    return "25"
+}
+const cgpa= 3.70;
+
+
+//NORMAL EXPORT OPTION 
+// exports.getName = getName;
+// exports.getAge = getAge;
+// exports.cgpa = cgpa;
+
+
+//MODULE EXPORT
+
+// module.exports ={
+//     getName,
+//     getAge,
+//     cgpa,
+// }jnhjhjh
